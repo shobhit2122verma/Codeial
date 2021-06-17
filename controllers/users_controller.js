036e71e7bfc4e58a0a5e93bb3c3ae9ee2b0aacd3
shobhit2122verma <44787115+shobhit2122verma@users.noreply.github.com>
2021-06-18 00:18:57 +0530

    added a form in our login page

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -16,3 +16,12 @@ module.exports.signIn=function(req,res){
 module.exports.signUp=function(req,res){
     res.render('user_sign_up',{title:"Codeial | Sign Up"});
 };
+
+//get the sign up data
+module.exports.create=function(req,res){
+    //TO DO
+};
+
+module.exports.createSession=function(req,res){
+    //TO DO
+};
